f35075271b38275eb025da785e680a0b011bd95b
microsoft <microsoft@brownsberger.net>
2025-06-14 08:23:10 -0400

    correcting escape instructions for environment settings.

--- legcrm-config.php
+++ legcrm-config.php
@@ -47,7 +47,7 @@ if ( 'PROD' == CRM_ENVIRONMENT ) {
 * 
 * DEFINE PRODUCTION PARAMETERS AS ENVIRONMENT VARIABLES IN AZURE WEB APP
 * https://learn.microsoft.com/en-us/azure/app-service/configure-common?tabs=portal
-* Everything other than letters, numbers, periods and underscores need to be escaped with back slash
+* 
 * 
 *
 * CAN ALTERNATIVELY HARD CODE WHEN TESTING
@@ -67,7 +67,6 @@ if ( 'PROD' == CRM_ENVIRONMENT ) {
 		
 		The following string encodes the array below:
 		[{"layerId":"senate","layerTitle":"Senate Districts","layerURL":"https:\/\/your_senatedistricts.geojson","link":"URL","featureTitle":"SENATOR","legend":"SEN_DIST","strokeColor":"#0000ff","strokeWeight":3,"strokeOpacity":0.2},{"layerId":"house","layerTitle":"House Districts","layerURL":"https:\/\/your_housedistrict.geojson","link":"URL","featureTitle":"REP","legend":"REP_DIST","strokeColor":"#ff0000","strokeWeight":2,"strokeOpacity":0.5},{"layerId":"muni","layerTitle":"Municipalities","layerURL":"https:\/\/your_municipalities.geojson","link":false,"featureTitle":"TOWN","legend":"POP2010","strokeColor":"#444","strokeWeight":4,"strokeOpacity":0.2}]
-		In environment setting, need to escape further: \[\{\"layerId\"\:\"senate\"\,\"layerTitle\"\:\"Senate Districts\"\,\"layerURL\"\:\"https\:\\\/\\\/your_senatedistricts.geojson\"\,\"link\"\:\"URL\"\,\"featureTitle\"\:\"SENATOR\"\,\"legend\"\:\"SEN_DIST\"\,\"strokeColor\"\:\"#0000ff\"\,\"strokeWeight\"\:3\,\"strokeOpacity\"\:0.2\}\,\{\"layerId\"\:\"house\"\,\"layerTitle\"\:\"House Districts\"\,\"layerURL\"\:\"https\:\\\/\\\/your_housedistrict.geojson\"\,\"link\"\:\"URL\"\,\"featureTitle\"\:\"REP\"\,\"legend\"\:\"REP_DIST\"\,\"strokeColor\"\:\"#ff0000\"\,\"strokeWeight\"\:2\,\"strokeOpacity\"\:0.5\}\,\{\"layerId\"\:\"muni\"\,\"layerTitle\"\:\"Municipalities\"\,\"layerURL\"\:\"https\:\\\/\\\/your_municipalities.geojson\"\,\"link\"\:false\,\"featureTitle\"\:\"TOWN\"\,\"legend\"\:\"POP2010\"\,\"strokeColor\"\:\"#444\"\,\"strokeWeight\"\:4\,\"strokeOpacity\"\:0.2\}\]
 		array (
 			array( 'layerId' =>'senate', 'layerTitle' => 'Senate Districts', 'layerURL' => 'https://your_senatedistricts.geojson', 'link' => 'URL', 'featureTitle' => 'SENATOR', 'legend' => 'SEN_DIST', 'strokeColor' => '#0000ff', 'strokeWeight' => 3, 'strokeOpacity' => .2),
 			array( 'layerId' =>'house', 'layerTitle' => 'House Districts', 'layerURL' => 'https://your_housedistrict.geojson',  'link' => 'URL', 'featureTitle' => 'REP', 'legend' => 'REP_DIST', 'strokeColor' => '#ff0000', 'strokeWeight' => 2, 'strokeOpacity' => .5),
@@ -104,7 +103,7 @@ if ( 'PROD' == CRM_ENVIRONMENT ) {
 */
 define('NONCE_KEY',getenv('NONCE_KEY')); // USE YOUR FAVORITE RANDOM LONG STRING GENERATOR 
 /* include as long using geojson files */
-define( 'WP_ISSUES_CRM_MAP_DATA_CREDIT', getenv('WP_ISSUES_CRM_MAP_DATA_CREDIT')); /*For example:  'Boundary Layers from \<a href=\"https\:\/\/www.mass.gov\/orgs\/massgis-bureau-of-geographic-information\" target = \"_blank\"\>MassGIS\<\/a\> converted using \<a href=\"https:\/\/www.macgis.com\/\" target=\"_blank\"\>Cartographica\<\/a\> and \<a href=\"https\:\/\/mygeodata.cloud\" target=\"_blank\"\>mygeodata.cloud\<\/\a\>.'
+define( 'WP_ISSUES_CRM_MAP_DATA_CREDIT', getenv('WP_ISSUES_CRM_MAP_DATA_CREDIT')); /*For example:  Boundary Layers from <a href="https://www.mass.gov/orgs/massgis-bureau-of-geographic-information" target = "_blank">MassGIS</a> converted using <a href="https://www.macgis.com/" target="_blank">Cartographica</a> and <a href="https://mygeodata.cloud" target="_blank">mygeodata.cloud</a>.
 /*
 * resource limits
 */
